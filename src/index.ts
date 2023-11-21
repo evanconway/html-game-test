154ce95f827e216e6156eeb14fda3f4a2c080d78
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-11-21 01:00:16 -0500

    consolidate logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 const GAME_WIDTH = 320;
 const GAME_HEIGHT = 180;
 
-const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d')!;
+const canvasImages: HTMLCanvasElement = document.getElementById('canvas-images') as HTMLCanvasElement;
+const ctxImages = canvasImages.getContext('2d')!;
 
-canvas.width = GAME_WIDTH;
-canvas.height = GAME_HEIGHT;
+canvasImages.width = GAME_WIDTH;
+canvasImages.height = GAME_HEIGHT;
 
 /**
  * Scales the game window to the maximum amount given the window size. Note that
@@ -16,11 +16,11 @@ canvas.height = GAME_HEIGHT;
 const resize = () => {
     const heightCalculatedFromWidth = window.innerWidth * (GAME_HEIGHT / GAME_WIDTH);
     if (heightCalculatedFromWidth <= window.innerHeight) {
-        canvas.style.width = Math.floor(window.innerWidth) + 'px';
-        canvas.style.height = Math.floor(heightCalculatedFromWidth) + 'px';
+        canvasImages.style.width = Math.floor(window.innerWidth) + 'px';
+        canvasImages.style.height = Math.floor(heightCalculatedFromWidth) + 'px';
     } else {
-        canvas.style.width = Math.floor(window.innerHeight * (GAME_WIDTH / GAME_HEIGHT)) + 'px';
-        canvas.style.height = Math.floor(window.innerHeight) + 'px';
+        canvasImages.style.width = Math.floor(window.innerHeight * (GAME_WIDTH / GAME_HEIGHT)) + 'px';
+        canvasImages.style.height = Math.floor(window.innerHeight) + 'px';
     }
 };
 window.addEventListener('resize', resize);
@@ -53,24 +53,31 @@ document.addEventListener('keyup', e => {
 
 const playerPos = { x: 0, y: 0 };
 
-const animate = () => {
+const gameUpdate = () => {
     const vel = 0.5;
     if (controller.up) playerPos.y -= vel;
     if (controller.down) playerPos.y += vel;
     if (controller.left) playerPos.x -= vel;
     if (controller.right) playerPos.x += vel;
-
     if (!controller.up && !controller.down && !controller.left && !controller.right) {
         playerPos.x = Math.round(playerPos.x);
         playerPos.y = Math.round(playerPos.y);
     }
+};
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(image, Math.round(playerPos.x), Math.round(playerPos.y));
-    ctx.fillStyle = 'black';
-    ctx.fillText('hello world', 50, 50);
+const clearCanvasImage = () => {
+    ctxImages.clearRect(0, 0, canvasImages.width, canvasImages.height);
+    ctxImages.fillStyle = 'white';
+    ctxImages.fillRect(0, 0, canvasImages.width, canvasImages.height);
+};
+
+const animate = () => {
+    clearCanvasImage();
+    gameUpdate();
+
+    ctxImages.drawImage(image, Math.round(playerPos.x), Math.round(playerPos.y));
+    ctxImages.fillStyle = 'black';
+    ctxImages.fillText('hello world', 50, 50);
     requestAnimationFrame(animate);
 };
 
